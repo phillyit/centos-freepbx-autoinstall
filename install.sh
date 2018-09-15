442d8652d1b46647a039f3dd4b899cfb99dd9ad0
phillyit <43277620+phillyit@users.noreply.github.com>
2018-09-14 20:37:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ tar xvfz asterisk-15-current.tar.gz
 rm -f asterisk-15-current.tar.gz
 cd asterisk-*
 contrib/scripts/install_prereq install
-sh configure
+make menuselect
 make
 make install
 make config
